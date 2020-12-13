bb3473165bcf6d8b84e25a33c5a27b7572e19bdb
pyk <x24870@gmail.com>
2020-12-13 17:23:55 +0800

    able to add product to cart in views

--- store/views.py
+++ store/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 from django.http import JsonResponse
 
@@ -34,4 +36,22 @@ def checkout(request):
     return render(request, 'store/checkout.html', context=context)
 
 def update_item(request):
+    data = json.loads(request.body)
+    productId = data['productId']
+    action = data['action']
+    customer = request.user.customer
+    product = Product.objects.get(id=productId)
+    order, created = Order.objects.get_or_create(customer=customer, complete=False)
+    orderItem, created = OrderItem.objects.get_or_create(order=order, product=product)
+
+
+    if action == 'add':
+        orderItem.quantity += 1
+    elif action == 'remove':
+        orderItem -= 1
+    orderItem.save()
+
+    if orderItem.quantity <= 0:
+        orderItem.delete()
+
     return JsonResponse('Item was added', safe=False)
