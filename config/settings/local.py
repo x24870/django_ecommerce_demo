fc03f4af3b849385387f78403c1df867b507cca4
pyk <x24870@gmail.com>
2020-12-14 21:35:35 +0800

    try to fix status 500 issue

--- config/settings/local.py
+++ config/settings/local.py
@@ -11,7 +11,7 @@ SECRET_KEY = env(
     default="tLFGLBhy2H9gNOAsXy21tCoJtBe7WeOZNA1QYvp0L5q0Vj6wbEEmhr5yAUckdK2N",
 )
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", "pyk-ecommerce.herokuapp.com"]
+ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1", ".pyk-ecommerce.herokuapp.com"]
 
 # CACHES
 # ------------------------------------------------------------------------------
