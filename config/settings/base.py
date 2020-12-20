7dff19d6235563f79d50edb93ebadab990143d38
pyk <x24870@gmail.com>
2020-12-20 16:55:27 +0800

    change SITE_ID to 1

--- config/settings/base.py
+++ config/settings/base.py
@@ -28,7 +28,7 @@ TIME_ZONE = "UTC"
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = "en-us"
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
-SITE_ID = 3
+SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
 USE_I18N = True
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-l10n
