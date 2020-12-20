8d3c65d464d2f0a45ce47987104dd7167951d557
pyk <x24870@gmail.com>
2020-12-20 16:40:27 +0800

    auto create customer when User doesn't have a Customer

--- store/utils.py
+++ store/utils.py
@@ -46,7 +46,15 @@ def cookieCart(request):
 
 def cartData(request):
     if request.user.is_authenticated:
-        customer = request.user.customer
+        customer, created = Customer.objects.get_or_create(
+            user=request.user, 
+            )
+        if not created:
+            print('Created new customer.')
+            customer.email=request.user.email,
+            customer.name=request.user.name
+            customer.save()
+
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
         items = order.orderitem_set.all()
         cartItems = order.get_cart_items
